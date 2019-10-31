80b89a18bff0500333b846accc2df65aa6def9b2
Finn <anthony.torres0926@gmail.com>
2019-10-31 11:00:56 -0600

    Completed.

--- js/main.js
+++ js/main.js
@@ -4,25 +4,25 @@ var removed = [];
 
 
 
-$('#button').on('click', function(evt) {
-    var skill = $(this).closest('p');
-    removed.push(skill[0].outerHTML);
-
-    skill.fadeOut(300, function() {
-        skill.remove();
-    });
-
-});
-
-
 $('#addSkill').on('click', function(evt) {
     let inputFind = evt.target.parentElement.children[0].value;
     inputArr.push(inputFind);
 
     var newSkill = `<div class="para" id="para">
-                    <p><button id="button" class="btn btn-danger">X</button> ${inputFind}</p>
+                    <p><button class="btn-danger">X</button> ${inputFind}</p>
                     </div>`;
     $('#para').append(newSkill);
+
+
+    $('.btn-danger').on('click', function(evt) {
+        var skill = $(this).closest('p');
+        removed.push(skill[0].outerHTML);
+    
+        skill.fadeOut(300, function() {
+            skill.remove();
+        });
+    
+    });
 
 
     // console.log(inputArr);
